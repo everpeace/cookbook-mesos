f913b2545252614f5105015f4957f17de49ed12c
Shingo Omura <everpeace@gmail.com>
2013-08-04 23:29:17 -0700

    correct small mistake(attributes -> attribute) in metadeta.rb

--- metadata.rb
+++ metadata.rb
@@ -14,91 +14,91 @@ depends          'java'
 depends          'python'
 depends          'build-essential'
 
-attributes          "mesos/version",
+attribute           "mesos/version",
   :recipes       => ["mesos::install"],
   :display_name  => "Version to be installed.",
   :description   => "branch name or tag name at http://github.com/apache/mesos",
   :default       => "master"
 
-attributes          "mesos/prefix",
+attribute           "mesos/prefix",
   :recipes       => ["mesos::install", "mesos::master", "mesos::slave"],
   :display_name  => "Prefix value to be passed to configure script",
   :description   => "prefix value to be passed to configure script",
   :default       => "/usr/local"
 
-attributes          "mesos/build/skip_test",
+attribute           "mesos/build/skip_test",
   :recipes       => ["mesos::install"],
   :display_name  => "Flag whether test will be performed.",
   :description   => "if true, test will be skipped.",
   :default       => "true"
 
-attributes          "mesos/ssh_opts",
+attribute           "mesos/ssh_opts",
   :recipes       => ["mesos::master"],
   :display_name  => "ssh options",
   :description   => "passed to be mesos-deploy-env.sh",
   :default       => "-o StrictHostKeyChecking=no -o ConnectTimeout=2"
 
-attributes          "mesos/deploy_with_sudo",
+attribute           "mesos/deploy_with_sudo",
   :recipes       => ["mesos::master"],
   :display_name  => "Flag whether sudo will be used in mesos deploy scripts",
   :description   => "Flag whether sudo will be used in mesos deploy scripts",
   :default       => "1"
 
-attributes          "mesos/cluster_name",
+attribute           "mesos/cluster_name",
   :recipes       => ["mesos::master"],
   :display_name  => "cluster name",
   :description   => "Human readable name for the cluster, displayed at webui"
 
-attributes          "mesos/master/zk",
+attribute           "mesos/master/zk",
   :recipes       => ["mesos::master"],
   :display_name  => "zookeeper url",
   :description   => "ZooKeeper URL (used for leader election amongst masters)"
 
-attributes          "mesos/master_ips",
+attribute           "mesos/master_ips",
   :recipes       => ["mesos::master"],
   :display_name  => "IP list of masters",
   :description   => "used in mesos-start/stop-cluster scripts."
 
-attributes          "mesos/slave_ips",
+attribute           "mesos/slave_ips",
   :recipes       => ["mesos::master"],
   :display_name  => "IP list of slaves",
   :description   => "used in mesos-start/stop-cluster scripts."
 
-attributes          "mesos/master/ip",
+attribute           "mesos/master/ip",
   :recipes       => ["mesos::master"],
   :display_name  => "mater listen ip.",
   :description   => "IP address to listen on"
 
-attributes          "mesos/master/log_dir",
+attribute           "mesos/master/log_dir",
   :recipes       => ["mesos::master"],
   :display_name  => "log_dir for master.",
   :description   => "Location to put log files.",
   :default       => "/var/log/mesos"
 
-attributes          "mesos/slave/master_url",
+attribute           "mesos/slave/master_url",
   :required      => "required",
   :recipes       => ["mesos::slave"],
   :display_name  => "master url",
   :description   => "mesos master url. this should  be host:port for non-ZooKeeper based masters, otherwise a zk:// or file://."
 
-attributes          "mesos/slave/ip",
+attribute           "mesos/slave/ip",
   :recipes       => ["mesos::slave"],
   :display_name  => "slave listen ip.",
   :description   => "IP address to listen on"
 
-attributes          "mesos/slave/log_dir",
+attribute           "mesos/slave/log_dir",
   :recipes       => ["mesos::slave"],
   :display_name  => "log_dir for slave.",
   :description   => "Location to put log files.",
   :default       => "/var/log/mesos"
 
-attributes          "mesos/slave/work_dir",
+attribute           "mesos/slave/work_dir",
   :recipes       => ["mesos::slave"],
   :display_name  => "work_dir for slave.",
   :description   => "Where to place framework work directories.",
   :default       => "/var/run/mesos"
 
-attributes          "mesos/slave/isolation",
+attribute           "mesos/slave/isolation",
   :recipes       => ["mesos::slave"],
   :display_name  => "Resource isolation mechanism.",
   :description   => "Isolation mechanism, may be one of: process, cgroups",
