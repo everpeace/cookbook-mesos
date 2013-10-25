74d1e3a9b322f0be9449f36dada5b21ec33cb57f
Shingo Omura <everpeace@gmail.com>
2013-10-25 12:00:13 -0700

    fix a bug for master, slave recipes

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -12,7 +12,7 @@ deploy_dir = File.join(prefix, "var", "mesos", "deploy")
 installed = File.exists?(File.join(prefix, "sbin", "mesos-master"))
 
 if !installed then
-  include_recipe "mesos::install"
+  include_recipe "mesos::build_from_source"
 end
 
 template File.join(deploy_dir, "mesos-deploy-env.sh") do
