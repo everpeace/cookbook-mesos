013cd1a2678e479a1c7cf84878928b4b2c56a2dc
Shingo Omura <everpeace@gmail.com>
2014-09-13 02:16:27 -0700

    version 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'everpeace@gmail.com'
 license          'MIT'
 description      'Installs/Configures mesos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.0'
 supports         'ubuntu', '>= 12.04'
 recipe           "mesos::default", "install mesos."
 recipe           "mesos::mesosphere", "install mesos from mesosphere package."
