d4ae8311df5aca54011006caf2ee8cd306a3b3eb
Jeff Byrnes <jeff@evertrue.com>
2015-07-01 18:05:52 -0400

    Refactor mesos::source helpers into recipe
    Like 52d96b2, there is no need for single-use methods for these resources.
    
    * Remove the helpers library entirely
    * Drop unneeded default values from resources
    * Ensure templates are always managed, regardless of whether or not
      Mesos has been installed (b/c idempotency)
    * DRY up per-role config templates
        * Works well with the services
    * Use execute resources in place of bash
        * Simpler & cleaner to just use the execute resource
    * Drop unnecessary code:
        * Parens are extraneous
        * Mode is, by default, 644

--- recipes/source.rb
+++ recipes/source.rb
@@ -3,88 +3,82 @@
 # Recipe:: install
 #
 
-::Chef::Recipe.send(:include, ::Helpers::Mesos)
-::Chef::Recipe.send(:include, ::Helpers::Source)
-Chef::Resource::Bash.send(:include, ::Helpers::Mesos)
-Chef::Resource::Bash.send(:include, ::Helpers::Source)
-Chef::Resource::RemoteFile.send(:include, ::Helpers::Mesos)
-Chef::Resource::RemoteFile.send(:include, ::Helpers::Source)
-Chef::Resource::Template.send(:include, ::Helpers::Mesos)
-Chef::Resource::Template.send(:include, ::Helpers::Source)
-Chef::Resource::Service.send(:include, ::Helpers::Mesos)
-Chef::Resource::Service.send(:include, ::Helpers::Source)
-
-if !(installed?) then
-  include_dependency_recipes
-  install_dependency_packages
-
-  remote_file "#{Chef::Config[:file_cache_path]}/mesos-#{mesos_version}.zip" do
-    source "#{download_url}"
-    mode   "0644"
-  end
-
-  bash "extracting mesos to #{node[:mesos][:home]}" do
-    cwd    "#{node[:mesos][:home]}"
-    code   <<-EOH
-      unzip -o #{Chef::Config[:file_cache_path]}/mesos-#{mesos_version}.zip -d ./
-      mv mesos-#{mesos_version} mesos
-    EOH
-    action :run
-  end
-
-  bash "building mesos from source" do
-    cwd   "#{node[:mesos][:home]}/mesos"
-    code  <<-EOH
-      ./bootstrap
-      ./bootstrap
-      mkdir -p build
-      cd build
-      ../configure --prefix=#{prefix}
-      make
-    EOH
-    action :run
-  end
-
-  bash "testing mesos" do
-    cwd    "#{node[:mesos][:home]}/mesos/build"
-    code   "make check"
-    action :run
-    only_if { node[:mesos][:build][:skip_test]==false }
-  end
-
-  bash "install mesos to #{prefix}" do
-    cwd    "#{node[:mesos][:home]}/mesos/build"
-    code   <<-EOH
-      make install
-      ldconfig
-    EOH
-    action :run
-  end
-
-  # configuration files for upstart scripts by source installation
-  template "/etc/init/mesos-master.conf" do
-    source "upstart.conf.for.buld_from_source.erb"
-    variables(:init_state => "stop", :role => "master")
-    mode 0644
-    owner "root"
-    group "root"
-    notifies :reload, "service[mesos-master]", :delayed
-  end
-
-  template "/etc/init/mesos-slave.conf" do
-    source "upstart.conf.for.buld_from_source.erb"
-    variables(:init_state => "stop", :role => "slave")
-    mode 0644
-    owner "root"
-    group "root"
-    notifies :reload, "service[mesos-slave]", :delayed
-  end
-else
-  Chef::Log.info("Mesos is already installed!! Instllation will be skipped.")
+%w(
+  python
+  build-essential
+  maven
+).each do |r|
+  include_recipe r
 end
 
-%w(master slave).each do |service|
-  service "mesos-#{service}" do
+case node["platform"]
+when "centos"
+  pkgs = %w(
+    python-devel
+    zlib-devel
+    libcurl-devel
+    openssl-devel
+    cyrus-sasl-devel
+    cyrus-sasl-md5
+  )
+when "ubuntu"
+  pkgs = %w(
+    unzip
+    libtool
+    libltdl-dev
+    autoconf
+    automake
+    libcurl3
+    libcurl3-gnutls
+    libcurl4-openssl-dev
+    libsasl2-dev
+  )
+end
+
+pkgs.each do |pkg|
+  package pkg
+end
+
+mesos_version = node[:mesos][:version]
+prefix = node[:mesos][:prefix]
+cmd = "#{prefix}/mesos-master --version |cut -f 2 -d ' '"
+
+unless File.exist?("#{prefix}/mesos-master") && (`#{cmd}`.chop == mesos_version)
+  remote_file "#{Chef::Config[:file_cache_path]}/mesos-#{mesos_version}.zip" do
+    source "https://github.com/apache/mesos/archive/#{mesos_version}.zip"
+  end
+
+  execute "extract mesos to #{node[:mesos][:home]}" do
+    cwd    "#{node[:mesos][:home]}"
+    command "unzip -o #{Chef::Config[:file_cache_path]}/mesos-#{mesos_version}.zip -d ./" \
+             " && mv mesos-#{mesos_version} mesos"
+  end
+
+  execute 'build mesos from source' do
+    cwd     "#{node[:mesos][:home]}/mesos"
+    command "./bootstrap && mkdir -p build && cd build && ../configure --prefix=#{prefix} && make"
+  end
+
+  execute 'test mesos' do
+    cwd     "#{node[:mesos][:home]}/mesos/build"
+    command 'make check'
+    not_if  { node[:mesos][:build][:skip_test] }
+  end
+
+  execute "install mesos to #{prefix}" do
+    cwd     "#{node[:mesos][:home]}/mesos/build"
+    command 'make install && ldconfig'
+  end
+end
+
+%w(master slave).each do |role|
+  template "/etc/init/mesos-#{role}.conf" do
+    source "upstart.conf.for.#{node[:mesos][:type]}.erb"
+    variables :init_state => "stop", :role => role
+    notifies :reload, "service[mesos-#{role}]"
+  end
+
+  service "mesos-#{role}" do
     provider Chef::Provider::Service::Upstart
     supports :restart => true, :reload => true
     action :nothing
