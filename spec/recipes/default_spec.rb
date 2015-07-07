93dbb06c683c4145f0ccf471443392eee7e09ded
Jeff Byrnes <jeff@evertrue.com>
2015-07-07 17:04:39 -0400

    Update ChefSpec for mesos::default
    * Checks for exceptions thrown by incorrect attribute settings
    * Checks for default attribute recipe inclusions
    * Check for different OS variations (apt vs. yum)
    * Checks for alternative attribute value recipe inclusions
    * Add standard header for default spec

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -1,23 +1,67 @@
-# encoding: utf-8
+#
+# Cookbook Name:: mesos
+# Spec:: default
+#
 
 require 'spec_helper'
 
 describe 'mesos::default' do
-  let :chef_run do
-    ChefSpec::ServerRunner.new do |node|
-      node.set[:mesos][:type] = :bork
+  context 'when platform is neither `ubuntu` nor `centos`' do
+    let :chef_run do
+      ChefSpec::ServerRunner.new(platform: 'debian', version: '7.0').converge described_recipe
+    end
+
+    it 'exits the Chef run' do
+      expect { chef_run }.to raise_error.with_message(/is not supported/)
     end
   end
 
-  before do
-    allow(Chef::Application).to receive(:fatal!)
+  context 'when node[:mesos][:type] is neither `source` nor `mesosphere`' do
+    let :chef_run do
+      ChefSpec::ServerRunner.new do |node|
+        node.set[:mesos][:type] = 'bork'
+      end.converge described_recipe
+    end
+
+    it 'exits the Chef run' do
+      expect { chef_run }.to raise_error.with_message(/should be 'source' or 'mesosphere'/)
+    end
   end
 
-  context 'when type is neither `source` or `mesosphere`' do
-    it 'exits the Chef run' do
-      chef_run.converge(described_recipe)
+  context 'when all attributes are default, on CentOS 6.6' do
+    let :chef_run do
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.6')
+      runner.converge(described_recipe)
+    end
 
-      expect(Chef::Application).to have_received(:fatal!).with(/should be 'source' or 'mesosphere'/)
+    it 'includes the `yum::default` recipe' do
+      expect(chef_run).to include_recipe 'yum'
+    end
+  end
+
+  context 'when all attributes are default, on Ubuntu 14.04' do
+    let(:chef_run) { ChefSpec::ServerRunner.new.converge described_recipe }
+
+    %w(
+      apt
+      java
+      mesos::source
+    ).each do |r|
+      it "includes the #{r} recipe" do
+        expect(chef_run).to include_recipe r
+      end
+    end
+  end
+
+  context 'when attribute [:mesos][:type] == mesosphere' do
+    let :chef_run do
+      ChefSpec::ServerRunner.new do |node|
+        node.set[:mesos][:type] = 'mesosphere'
+      end.converge described_recipe
+    end
+
+    it 'includes the mesosphere recipe' do
+      expect(chef_run).to include_recipe 'mesos::mesosphere'
     end
   end
 end
