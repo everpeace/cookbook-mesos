385d7216247127d8c0db10c376c9211dcec4e85b
Jeff Byrnes <jeff@evertrue.com>
2015-07-08 16:54:11 -0400

    Refactor mesos::master ChefSpec tests
    Appropriately covers just the mesos::master recipe, in its various
    iterations.

--- spec/recipes/master_spec.rb
+++ spec/recipes/master_spec.rb
@@ -1,85 +1,172 @@
-# encoding: utf-8
+#
+# Cookbook Name:: mesos
+# Spec:: master
+#
 
 require 'spec_helper'
 
 describe 'mesos::master' do
-  include_context 'setup context'
+  deploy_dir = '/usr/local/var/mesos/deploy'
 
-  shared_examples_for 'a master recipe' do
-    it 'creates deploy dir' do
-      expect(chef_run).to create_directory '/usr/local/var/mesos/deploy'
+  context 'when node[:mesos][:master][:zk] is not set' do
+    let(:chef_run) { ChefSpec::ServerRunner.new.converge described_recipe }
+
+    it 'exits the Chef run' do
+      expect { chef_run }.to raise_error.with_message(
+        'node[:mesos][:master][:zk] is required to configure mesos-master.'
+      )
+    end
+  end
+
+  context 'when node[:mesos][:master][:zk] is set, but node[:mesos][:master][:zk] is not set' do
+    let :chef_run do
+      ChefSpec::ServerRunner.new do |node|
+        node.set[:mesos][:master][:zk] = 'zk-string'
+      end.converge described_recipe
     end
 
-    describe 'masters file' do
-      it 'creates it in deploy directory' do
-        expect(chef_run).to create_template '/usr/local/var/mesos/deploy/masters'
-      end
+    it 'exits the Chef run' do
+      expect { chef_run }.to raise_error.with_message(
+        'node[:mesos][:master][:quorum] is required to configure mesos-master.'
+      )
+    end
+  end
 
-      it 'contains configured master IPs' do
-        expect(chef_run).to render_file('/usr/local/var/mesos/deploy/masters')
-          .with_content(/^#{Regexp.escape('10.0.0.1')}$/)
-      end
+  context(
+    'when node[:mesos][:master][:zk] & node[:mesos][:master][:quorum] are set, ' \
+    'but all other attributes are default, on Ubuntu 14.04'
+  ) do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new do |node|
+        node.set[:mesos][:master][:zk] = 'zk-string'
+        node.set[:mesos][:master][:quorum] = '1'
+      end.converge described_recipe
     end
 
-    describe 'slaves file' do
-      it 'creates it in deploy directory' do
-        expect(chef_run).to create_template '/usr/local/var/mesos/deploy/slaves'
-      end
+    it 'includes mesos::default' do
+      expect(chef_run).to include_recipe 'mesos::default'
+    end
 
-      it 'contains configured slave IPs' do
-        expect(chef_run).to render_file('/usr/local/var/mesos/deploy/slaves')
-          .with_content(/^#{Regexp.escape('10.0.0.4')}$/)
-      end
+    it 'does nothing to service[mesos-master]' do
+      resource = chef_run.service 'mesos-master'
+      expect(resource).to do_nothing
+    end
+
+    it 'creates the Mesos deploy dir' do
+      expect(chef_run).to create_directory(deploy_dir).with(
+        recursive: true
+      )
+    end
+
+    it 'creates the masters config file' do
+      expect(chef_run).to create_template "#{deploy_dir}/masters"
+    end
+
+    it 'creates the slaves config file' do
+      expect(chef_run).to create_template "#{deploy_dir}/slaves"
     end
 
     describe 'deploy env file' do
       it 'creates it' do
-        expect(chef_run).to create_template '/usr/local/var/mesos/deploy/mesos-deploy-env.sh'
+        expect(chef_run).to create_template "#{deploy_dir}/mesos-deploy-env.sh"
       end
 
       it 'contains SSH_OPTS variable' do
-        expect(chef_run).to render_file('/usr/local/var/mesos/deploy/mesos-deploy-env.sh')
+        expect(chef_run).to render_file("#{deploy_dir}/mesos-deploy-env.sh")
           .with_content(/^export SSH_OPTS="#{Regexp.escape('-o StrictHostKeyChecking=no -o ConnectTimeout=2')}"$/)
       end
 
       it 'contains DEPLOY_WITH_SUDO variable' do
-        expect(chef_run).to render_file('/usr/local/var/mesos/deploy/mesos-deploy-env.sh')
+        expect(chef_run).to render_file("#{deploy_dir}/mesos-deploy-env.sh")
           .with_content(/^export DEPLOY_WITH_SUDO="1"$/)
       end
     end
 
     describe 'master env file' do
       it 'creates it' do
-        expect(chef_run).to create_template '/usr/local/var/mesos/deploy/mesos-master-env.sh'
+        expect(chef_run).to create_template "#{deploy_dir}/mesos-master-env.sh"
       end
 
-      it 'contains each key-value pair from node[:mesos][:master]' do
-        expect(chef_run).to render_file('/usr/local/var/mesos/deploy/mesos-master-env.sh')
-          .with_content(/^export MESOS_fake_key=fake_value$/)
+      it 'contains the ' do
+        expect(chef_run).to render_file("#{deploy_dir}/mesos-master-env.sh")
+          .with_content(%r{^export MESOS_work_dir=/tmp/mesos$})
       end
+
       it 'notifies service[mesos-master] to reload configurations and restart' do
-        conf = chef_run.template('/usr/local/var/mesos/deploy/mesos-master-env.sh')
+        conf = chef_run.template("#{deploy_dir}/mesos-master-env.sh")
+        expect(conf).to notify('service[mesos-master]').to(:reload)
+        expect(conf).to notify('service[mesos-master]').to(:restart)
+      end
+    end
+
+    describe 'mesos-master upstart script' do
+      it 'installs it to /etc/init' do
+        expect(chef_run).to create_template '/etc/init/mesos-master.conf'
+      end
+
+      it 'describe service name "mesos master"' do
+        expect(chef_run).to render_file('/etc/init/mesos-master.conf')
+          .with_content(/^description "mesos master"$/)
+      end
+
+      it 'contains "start on stopped rc with runlevel 2,3,4,5"' do
+        expect(chef_run).to render_file('/etc/init/mesos-master.conf')
+          .with_content(/^start on stopped rc RUNLEVEL=\[2345\]$/)
+      end
+
+      it 'contains "respawn"' do
+        expect(chef_run).to render_file('/etc/init/mesos-master.conf')
+          .with_content(/^respawn/)
+      end
+
+      it 'specifies the correct role' do
+        expect(chef_run).to render_file('/etc/init/mesos-master.conf')
+          .with_content 'role=master'
+      end
+
+      it 'notifies service[mesos-master] to reload service configuration' do
+        conf = chef_run.template('/etc/init/mesos-master.conf')
         expect(conf).to notify('service[mesos-master]').to(:reload).delayed
-        expect(conf).to notify('service[mesos-master]').to(:restart).delayed
       end
     end
   end
 
-  context 'when installed from mesosphere' do
+  context 'when master and slave IPs are specified in attributes' do
+    let :chef_run do
+      ChefSpec::ServerRunner.new do |node|
+        # These attributes are set to avoid failing the Chef run
+        node.set[:mesos][:master][:zk] = 'zk-string'
+        node.set[:mesos][:master][:quorum] = '1'
+        node.set[:mesos][:master_ips] = %w(10.0.0.1)
+        node.set[:mesos][:slave_ips] = %w(10.0.0.4)
+      end.converge described_recipe
+    end
+
+    it 'has a masters config with supplied IPs' do
+      expect(chef_run).to render_file("#{deploy_dir}/masters")
+        .with_content %r{^10.0.0.1$}
+    end
+
+    it 'has a slaves config with supplied IPs' do
+      expect(chef_run).to render_file("#{deploy_dir}/slaves")
+        .with_content %r{^10.0.0.4$}
+    end
+  end
+
+  context 'when node[:mesos][:type] == mesosphere, on Ubuntu 14.04' do
     let :chef_run do
       ChefSpec::ServerRunner.new do |node|
         node.set[:mesos][:type] = 'mesosphere'
         node.set[:mesos][:master][:zk] = 'zk-string'
         node.set[:mesos][:master][:quorum] = '1'
-        node.set[:mesos][:mesosphere][:with_zookeeper] = true
-        node.set[:mesos][:master_ips] = %w[10.0.0.1]
-        node.set[:mesos][:slave_ips] = %w[10.0.0.4]
         node.set[:mesos][:master][:fake_key] = 'fake_value'
       end.converge(described_recipe)
     end
 
-    it_behaves_like 'an installation from mesosphere', :init_master_state=>"start", :init_slave_state=>"stop"
-    it_behaves_like 'a master recipe'
+    it 'has a mesos-master upstart script with a different command' do
+      expect(chef_run).to render_file('/etc/init/mesos-master.conf')
+        .with_content(%r{^exec \/usr\/bin\/mesos-init-wrapper master$})
+    end
 
     describe '/etc/mesos/zk' do
       it 'creates it' do
@@ -99,10 +186,6 @@ describe 'mesos::master' do
       it 'contains LOGS variable' do
         expect(chef_run).to render_file('/etc/default/mesos').with_content(/^LOGS=\/var\/log\/mesos$/)
       end
-
-      it 'contains ULIMIT variable' do
-        expect(chef_run).to render_file('/etc/default/mesos').with_content(/^ULIMIT="-n 8192"$/)
-      end
     end
 
     describe '/etc/default/mesos-master' do
@@ -114,48 +197,27 @@ describe 'mesos::master' do
         expect(chef_run).to render_file('/etc/default/mesos-master')
           .with_content(/^PORT=5050$/)
       end
-
-      it 'contains ZK variable' do
-        expect(chef_run).to render_file('/etc/default/mesos-master')
-          .with_content(/^ZK=`cat \/etc\/mesos\/zk`$/)
-      end
     end
 
     it 'creates /etc/mesos-master' do
       expect(chef_run).to create_directory '/etc/mesos-master'
     end
 
-    it 'runs a cleanup of /etc/mesos-master/*' do
+    it 'deletes the contents of /etc/mesos-master' do
       expect(chef_run).to run_execute('rm -rf /etc/mesos-master/*')
     end
 
-    describe 'configuration options in /etc/mesos-master' do
-      it 'sets content to each key-value pair in node[:mesos][:master]' do
-        expect(chef_run).to render_file('/etc/mesos-master/work_dir')
-          .with_content(%r{^/tmp/mesos$})
+    describe 'configuration files in /etc/mesos-master' do
+      it 'sets the content of the file matching a key in node[:mesos][:master] to its corresponding value' do
         expect(chef_run).to render_file('/etc/mesos-master/quorum')
           .with_content(/^1$/)
+
+        expect(chef_run).to render_file('/etc/mesos-master/work_dir')
+          .with_content '/tmp/mesos'
+
         expect(chef_run).to render_file('/etc/mesos-master/fake_key')
           .with_content(/^fake_value$/)
       end
     end
-  end
-
-  context 'when installed from source' do
-    let :chef_run do
-      ChefSpec::ServerRunner.new do |node|
-        node.set[:mesos][:type] = 'source'
-        node.set[:mesos][:master][:zk] = 'zk-string'
-        node.set[:mesos][:master][:quorum] = '1'
-        node.set[:mesos][:mesosphere][:with_zookeeper] = true
-        node.set[:mesos][:master_ips] = %w[10.0.0.1]
-        node.set[:mesos][:slave_ips] = %w[10.0.0.4]
-        node.set[:mesos][:master][:fake_key] = 'fake_value'
-        node.set[:mesos][:build][:skip_test] = false
-      end.converge(described_recipe)
-    end
-
-    it_behaves_like 'an installation from source', :init_master_state => "start", :init_slave_state =>"stop"
-    it_behaves_like 'a master recipe'
   end
 end
