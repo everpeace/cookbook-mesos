03aeb15142632eff52a7296412fd1647d5feb2b9
Jeff Byrnes <jeff@evertrue.com>
2015-07-13 15:28:05 -0400

    Specify more current CentOS version in metadata
    We test on CentOS 6.6, so lets set the support there

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.2'
 
 supports         'ubuntu', '>= 14.04'
-supports         'centos', '>= 6.0'
+supports         'centos', '>= 6.6'
 
 recipe           "mesos::default", "install mesos."
 recipe           "mesos::mesosphere", "install mesos from mesosphere package."
