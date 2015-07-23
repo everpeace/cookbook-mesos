07ce6e610c657305f8115297afd855a905a49570
Jeff Byrnes <jeff@evertrue.com>
2015-07-23 12:08:58 -0400

    Ensure addition of all necessary CentOS yum repos

--- recipes/mesosphere.rb
+++ recipes/mesosphere.rb
@@ -9,14 +9,32 @@ case node['platform']
 when "centos"
   repo_url = value_for_platform(
     'centos' => {
-      'default' => 'http://repos.mesosphere.io/el/6/noarch/',
-      '~> 7.0' => 'http://repos.mesosphere.io/el/7/noarch/'
+      'default' => 'http://repos.mesosphere.io/el/6',
+      '~> 7.0' => 'http://repos.mesosphere.io/el/7'
     }
   )
 
-  yum_repository 'mesosphere-noarch' do
-    description 'Mesosphere repo'
-    baseurl repo_url
+  repos = {
+    'mesosphere' => {
+      'description' => 'Mesosphere Packages - $basearch',
+      'url' => '$basearch'
+    },
+    'mesosphere-noarch' => {
+      'description' => 'Mesosphere Packages - noarch',
+      'url' => 'noarch'
+    },
+    'mesosphere-source' => {
+      'description' => 'Mesosphere Packages - $basearch - Source',
+      'url' => 'SRPMS'
+    }
+  }
+
+  repos.each do |repo, details|
+    yum_repository repo do
+      description details['description']
+      baseurl "#{repo_url}/#{details['url']}/"
+      gpgkey 'http://repos.mesosphere.io/el/RPM-GPG-KEY-mesosphere'
+    end
   end
 
   yum_package "mesos >= #{node[:mesos][:version]}"
