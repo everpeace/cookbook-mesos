36bc27bcf43902fdce11813b15f77cbdb3e334f5
Jeff Byrnes <jeff@evertrue.com>
2015-07-23 12:09:18 -0400

    Test against platform-correct list of ZK packages

--- test/integration/helpers/serverspec/mesosphere_installation.rb
+++ test/integration/helpers/serverspec/mesosphere_installation.rb
@@ -3,8 +3,16 @@
 shared_examples_for 'an installation from mesosphere' do |opt|
   if(opt[:with_zookeeper])
     context 'with zookeeper' do
-      it 'installs zookeeper, zookeeperd, zookeeper-bin' do
-        ['zookeeper', 'zookeeperd', 'zookeeper-bin'].each do |zk|
+      it 'installs Zookeeper packages' do
+        puts "The OS family is #{os[:family]}"
+        case os[:family]
+        when 'centos', 'redhat'
+          pkgs = %w(zookeeper zookeeper-server)
+        when 'ubuntu', 'debian'
+          pkgs = %(zookeeper zookeeperd zookeeper-bin)
+        end
+
+        pkgs.each do |zk|
           expect(package(zk)).to be_installed
         end
       end
